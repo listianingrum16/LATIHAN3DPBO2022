25ada09ffa2de289e597a4bdb69662bb82fbd05e
Listia Ningrum <95666745+listianingrum16@users.noreply.github.com>
2022-02-26 10:38:33 +0700

    Create Processor.py

---
+++ Python/Processor.py
@@ -0,0 +1,25 @@
+# Saya Listia Ningrum NIM 2008084 mengerjakan Latihan 3 dalam mata
+# kuliah DPBO untuk keberkahanNya maka saya tidak akan melakukan kecurangan 
+# seperti yang telah dispesifikasikan. Aamiin 
+
+class Processor():
+
+    # deklarasi atribut
+    __name = ""
+    __price = 0
+
+    # constructor
+    def __init__(self):
+        self
+    
+    # method set & get name
+    def setName(self, name):
+        self.__name = name
+    def getName(self):
+        return self.__name
+		
+    # method set & get price
+    def setPrice(self, price):
+        self.__price = price
+    def getPrice(self):
+        return self.__price
